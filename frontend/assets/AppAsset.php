a1e2ae563c1635057634829b4e90938b8de4cac2
张洪波 <zhanghongbo@anlewo.com>
2017-12-02 10:53:18 +0800

    初始化 社区项目

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@frontend/assets';
+//    public $baseUrl = '@web';
+    public $css = [
+        'layui/css/layui.css',
+        'css/global.css',
+    ];
+    public $js = [
+        'layui/layui.js'
+    ];
+    public $depends = [
+
+    ];
+    public $jsOptions = [
+        'position' => \yii\web\View::POS_HEAD,   // 这是设置所有js放置的位置
+    ];
+}
